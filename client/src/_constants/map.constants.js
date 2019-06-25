0dd2f3c0cb4573855e9e96b2c41e253a2eab5ce8
v.dmitriev <vladyslav.dmitriev@gmail.com>
2019-06-26 00:08:52 +0400

    code review

--- client/src/_constants/map.constants.js
+++
@@ -1,7 +0,0 @@
-export const MAP_TILE_LAYER = 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png';
-export const MAP_START_ZOOM = 11;
-
-export default {
-  MAP_TILE_LAYER,
-  MAP_START_ZOOM,
-};
